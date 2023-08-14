140aa63e7c6c3a00702bb4a165119aca4d41a89b
Emon <emonhasan7650@gmail.com>
2023-08-15 02:20:54 +0600

    get all, single user, update and delete a user functionlity add sucessfully

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -2,37 +2,98 @@ const { generateToken } = require("../config/jwtToken");
 const User = require("../models/userModal");
 const asyncHandler = require("express-async-handler");
 
+//register a user
 const createUser = asyncHandler(async (req, res) => {
-    const email = req.body.email;
-    const findUser = await User.findOne({ email: email });
-    if (!findUser) {
-      //create a new user
-      const newUser = await User.create(req.body);
-      res.json(
-          newUser
-          );
-    } else {
-      //user already exists
-      throw new Error("User already exists")
-    }
-  });
+  const email = req.body.email;
+  const findUser = await User.findOne({ email: email });
+  if (!findUser) {
+    //create a new user
+    const newUser = await User.create(req.body);
+    res.json(newUser);
+  } else {
+    //user already exists
+    throw new Error("User already exists");
+  }
+});
 
-  const loginUserCtrl = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-    //check if user already exists or not
-    const findUser = await User.findOne({ email });
-    if (findUser && (await findUser.isPasswordMatched(password))) {
-      res.json({
-        _id: findUser?._id,
-        firstname: findUser?.firstname,
-        lastname: findUser?.lastname,
-        email: findUser?.email,
-        mobile: findUser?.mobile,
-        token: generateToken(findUser?._id)
-      });
-    } else {
-      throw new Error("Invalid Credentials");
-    }
-  })
+//Login a user
+const loginUserCtrl = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  //check if user already exists or not
+  const findUser = await User.findOne({ email });
+  if (findUser && (await findUser.isPasswordMatched(password))) {
+    res.json({
+      _id: findUser?._id,
+      firstname: findUser?.firstname,
+      lastname: findUser?.lastname,
+      email: findUser?.email,
+      mobile: findUser?.mobile,
+      token: generateToken(findUser?._id),
+    });
+  } else {
+    throw new Error("Invalid Credentials");
+  }
+});
 
-module.exports = { createUser, loginUserCtrl };
+//update a user
+const updateAUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updateAUser = await User.findByIdAndUpdate(
+      id,
+      {
+        firstname: req?.body?.firstname,
+        lastname: req?.body?.lastname,
+        email: req?.body?.email,
+        mobile: req?.body?.mobile,
+      },
+      {
+        new: true,
+      }
+    );
+    res.json(updateAUser);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+//Get all user
+const getAllUser = asyncHandler(async (req, res) => {
+  try {
+    const getUsers = await User.find();
+    res.json(getUsers);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+//Get a single user
+const getAUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const getUser = await User.findById(id);
+    res.json({ getUser });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+//Delete a single user
+const deleteAUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleteAUser = await User.findByIdAndDelete(id);
+    res.json({ deleteAUser });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = {
+  createUser,
+  loginUserCtrl,
+  getAllUser,
+  getAUser,
+  deleteAUser,
+  updateAUser,
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,19 @@
-const express = require('express');
-const { createUser, loginUserCtrl } = require('../controller/userCtrl');
+const express = require("express");
+const {
+  createUser,
+  loginUserCtrl,
+  getAllUser,
+  getAUser,
+  deleteAUser,
+  updateAUser,
+} = require("../controller/userCtrl");
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
+router.get("/all-users", getAllUser);
+router.get("/:id", getAUser);
+router.delete("/:id", deleteAUser);
+router.put("/:id", updateAUser);
 
 module.exports = router;
