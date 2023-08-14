1b1bb715fcc25c42a323fe4029d10c49ab40d103
Emon <emonhasan7650@gmail.com>
2023-08-14 17:27:33 +0600

    password generate for use bcrypt and role set

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,3 +1,4 @@
+const { generateToken } = require("../config/jwtToken");
 const User = require("../models/userModal");
 const asyncHandler = require("express-async-handler");
 
@@ -16,4 +17,22 @@ const createUser = asyncHandler(async (req, res) => {
     }
   });
 
-module.exports = { createUser };
+  const loginUserCtrl = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    //check if user already exists or not
+    const findUser = await User.findOne({ email });
+    if (findUser && (await findUser.isPasswordMatched(password))) {
+      res.json({
+        _id: findUser?._id,
+        firstname: findUser?.firstname,
+        lastname: findUser?.lastname,
+        email: findUser?.email,
+        mobile: findUser?.mobile,
+        token: generateToken(findUser?._id)
+      });
+    } else {
+      throw new Error("Invalid Credentials");
+    }
+  })
+
+module.exports = { createUser, loginUserCtrl };
