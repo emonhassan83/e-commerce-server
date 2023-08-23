dbf00f99812a5d6a2481e1c67370459c676f4d54
Emon <emonhasan7650@gmail.com>
2023-08-24 03:24:43 +0600

    product wishlist and reating features added

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -1,7 +1,9 @@
 const Product = require("../models/productModal");
+const User = require("../models/userModal");
 const asyncHandler = require("express-async-handler");
 const slugify = require("slugify");
 const validateMongodbId = require("../utils/validateMongodbId");
+const { response } = require("express");
 
 //create a new product
 const createProduct = asyncHandler(async (req, res) => {
@@ -18,16 +20,20 @@ const createProduct = asyncHandler(async (req, res) => {
 
 //update a product
 const updateAProduct = asyncHandler(async (req, res) => {
-    const productId = req.params.id;
-    validateMongodbId(productId); 
+  const productId = req.params.id;
+  validateMongodbId(productId);
   try {
     if (req.body.title) {
       req.body.slug = slugify(req.body.title);
     }
-    const updateProduct = await Product.findOneAndUpdate({ _id: productId }, req.body, {new:true});
+    const updateProduct = await Product.findOneAndUpdate(
+      { _id: productId },
+      req.body,
+      { new: true }
+    );
     if (!updateProduct) {
-        return res.status(404).json({ error: 'Product not found' });
-      }
+      return res.status(404).json({ error: "Product not found" });
+    }
     res.json(updateProduct);
   } catch (error) {
     throw new Error(error);
@@ -36,14 +42,14 @@ const updateAProduct = asyncHandler(async (req, res) => {
 
 //delete a product
 const deleteAProduct = asyncHandler(async (req, res) => {
-    const {id} = req.params;
-    try {
-      const deleteProduct = await Product.findByIdAndDelete(id);
-      res.json(deleteProduct);
-    } catch (error) {
-      throw new Error(error);
-    }
-  });
+  const { id } = req.params;
+  try {
+    const deleteProduct = await Product.findByIdAndDelete(id);
+    res.json(deleteProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
 //get a product
 const getAProduct = asyncHandler(async (req, res) => {
@@ -59,11 +65,10 @@ const getAProduct = asyncHandler(async (req, res) => {
 //get all products
 const getAllProducts = asyncHandler(async (req, res) => {
   try {
-
     //Products filtering
-    const queryObj = {...req.query};
+    const queryObj = { ...req.query };
     const excludeFields = ["page", "sort", "limit", "fields"];
-    excludeFields.forEach(el=> delete queryObj[el]);
+    excludeFields.forEach((el) => delete queryObj[el]);
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     let query = Product.find(JSON.parse(queryStr));
@@ -73,7 +78,7 @@ const getAllProducts = asyncHandler(async (req, res) => {
       const sortBy = req.query.sort.split(",").join(" ");
       query = query.sort(sortBy);
     } else {
-      query = query.sort('-createdAt');
+      query = query.sort("-createdAt");
     }
 
     //limiting the fields
@@ -81,10 +86,10 @@ const getAllProducts = asyncHandler(async (req, res) => {
       const fields = req.query.fields.split(",").join(" ");
       query = query.select(fields);
     } else {
-      query = query.select('-__v');
+      query = query.select("-__v");
     }
 
-    //pagination 
+    //pagination
     const page = req.query.page;
     const limit = req.query.limit;
     const skip = (page - 1) * limit;
@@ -92,9 +97,9 @@ const getAllProducts = asyncHandler(async (req, res) => {
     if (req.query.page) {
       const productCount = await Product.countDocuments();
       if (skip >= productCount) throw new Error("This page dose not exist");
-    } 
+    }
     //console.log(page, limit, skip);
-    
+
     const product = await query;
     res.json(product);
   } catch (error) {
@@ -102,4 +107,88 @@ const getAllProducts = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { createProduct, getAProduct, getAllProducts, updateAProduct, deleteAProduct };
+//add to wishlist
+const addToWishlist = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const { prodId } = req.body;
+  try {
+    const user = await User.findById(_id);
+    const alreadyAdded = user.wishlist.find((id) => id.toString() === prodId);
+    if (alreadyAdded) {
+      let user = await User.findByIdAndUpdate(
+        _id,
+        {
+          $pull: { wishlist: prodId },
+        },
+        { new: true }
+      );
+      res.json(user);
+    } else {
+      let user = await User.findByIdAndUpdate(
+        _id,
+        {
+          $push: { wishlist: prodId },
+        },
+        { new: true }
+      );
+      res.json(user);
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+//add to rating
+const addToRating = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const { star, prodId } = req.body;
+  try {
+    const product = await Product.findById(prodId);
+    let alreadyRated = product.ratings.find(
+      (userId) => userId.postedby.toString() === _id.toString()
+    );
+    if (alreadyRated) {
+      const updateRating = await Product.updateOne(
+        { ratings: { $elemMatch: alreadyRated } },
+        { $set: { "ratings.$.star": star } },
+        { new: true }
+      );
+    } else {
+      const rateProduct = await Product.findByIdAndUpdate(
+        prodId,
+        {
+          $push: {
+            ratings: { star: star, postedby: _id },
+          },
+        },
+        { new: true }
+      );
+    }
+
+    const getAllRatings = await Product.findById(prodId);
+    let totalRating = getAllRatings.ratings.length;
+    let ratingSum = getAllRatings.ratings.map((item) =>
+      item.star).reduce((prev, curr) => prev + curr, 0);
+    let actualRating = Math.round(ratingSum / totalRating);
+    let finalProduct = await Product.findByIdAndUpdate(
+      prodId,
+      {
+        totalRating: actualRating,
+      },
+      { new: true }
+    );
+    res.json(finalProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = {
+  createProduct,
+  getAProduct,
+  getAllProducts,
+  updateAProduct,
+  deleteAProduct,
+  addToWishlist,
+  addToRating,
+};
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -5,12 +5,17 @@ const {
   getAllProducts,
   updateAProduct,
   deleteAProduct,
+  addToWishlist,
+  addToRating,
 } = require("../controller/productCtrl");
 const router = express.Router();
 const { isAdmin, authMiddleware } = require("../middleware/authMiddleware");
 
 router.post("/", authMiddleware,isAdmin, createProduct);
 router.get("/:id", getAProduct);
+router.put("/wishlist", authMiddleware, addToWishlist);
+router.put("/rating", authMiddleware, addToRating);
+
 router.put("/:id", authMiddleware,isAdmin, updateAProduct);
 router.delete("/:id", authMiddleware,isAdmin, deleteAProduct);
 router.get("/", getAllProducts);
