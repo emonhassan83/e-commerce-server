23616b061a3279ae0acec70b43fa595d7aca9b96
Emon <emonhasan7650@gmail.com>
2023-08-27 01:23:28 +0600

    coupon CURD operation done

--- controller/couponCtrl.js
+++ controller/couponCtrl.js
@@ -1,14 +1,51 @@
 const Coupon = require("../models/couponModal");
 const asyncHandler = require("express-async-handler");
+const validateMongodbId = require("../utils/validateMongodbId");
 
-const createCoupon = asyncHandler(async(req,res)=> {
-    try {
-        const newCoupon = await Coupon.create(req.body);
-        res.json(newCoupon);
-    } catch (error) {
-        throw new Error(error);
-    }
-})
+//create a new coupon
+const createCoupon = asyncHandler(async (req, res) => {
+  try {
+    const newCoupon = await Coupon.create(req.body);
+    res.json(newCoupon);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
+//Get all coupon
+const getAllCoupon = asyncHandler(async (req, res) => {
+  try {
+    const coupons = await Coupon.find();
+    res.json(coupons);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
-module.exports = {createCoupon};
+//Update a coupon
+const updateCoupon = asyncHandler(async (req, res) => {
+    const {id} = req.params;
+    validateMongodbId(id);
+
+  try {
+    const updateCoupon = await Coupon.findByIdAndUpdate(id, req.body, {new: true});
+    res.json(updateCoupon);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+//Delete a coupon
+const deleteCoupon = asyncHandler(async (req, res) => {
+    const {id} = req.params;
+    validateMongodbId(id);
+
+  try {
+    const deleteCoupon = await Coupon.findByIdAndDelete(id);
+    res.json(deleteCoupon);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = { createCoupon, getAllCoupon, updateCoupon, deleteCoupon };
--- routes/couponRoute.js
+++ routes/couponRoute.js
@@ -1,7 +1,16 @@
-const express = require('express');
-const { createCoupon } = require('../controller/couponCtrl');
+const express = require("express");
+const {
+  createCoupon,
+  getAllCoupon,
+  updateCoupon,
+  deleteCoupon,
+} = require("../controller/couponCtrl");
+const { authMiddleware, isAdmin } = require("../middleware/authMiddleware");
 const router = express.Router();
 
-router.post("/", createCoupon);
+router.post("/", authMiddleware, isAdmin, createCoupon);
+router.get("/", authMiddleware, isAdmin, getAllCoupon);
+router.put("/:id", authMiddleware, isAdmin, updateCoupon);
+router.delete("/:id", authMiddleware, isAdmin, deleteCoupon);
 
 module.exports = router;
